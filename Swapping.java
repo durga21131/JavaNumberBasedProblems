f54957e514b1d9ae2ea9fc8dbf2ae8271c443716
durga21131 <durgaprasad21131@gmail.com>
2024-12-29 20:45:58 +0530

    new

---
+++ Swapping.java
@@ -0,0 +1,50 @@
+package NumberProblems;
+
+import java.util.Scanner;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		Swapping s = new Swapping();
+		// s.WithOutUsing3Variable();
+		// s.WithOutUsing3Variable2();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first Value");
+		int a = sc.nextInt();
+		System.out.println("Eneter second value ");
+		int b = sc.nextInt();
+		System.out.println("the values before swapping is:");
+		System.out.println("a value is: " + a + "   b value is " + b);
+		int c = a;
+		a = b;
+		b = a;
+		System.out.println("Values after swapping is:");
+		System.out.println("a value is: " + b + "   b value is " + a);
+
+	}
+
+	void WithOutUsing3Variable() {
+		int a = 10;
+		int b = 15;
+		System.out.println("the values before swapping is:");
+		System.out.println("a value is: " + a + "   b value is " + b);
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("the values after swapping is:");
+		System.out.println("a value is: " + a + "   b value is " + b);
+	}
+
+	void WithOutUsing3Variable2() {
+		int a = 10;
+		int b = 15;
+		System.out.println("the values before swapping is:");
+		System.out.println("a value is: " + a + "   b value is " + b);
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+		System.out.println("the values after swapping is:");
+		System.out.println("a value is: " + a + "   b value is " + b);
+	}
+}
