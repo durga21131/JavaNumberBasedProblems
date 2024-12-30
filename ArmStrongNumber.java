21cb84c53dae62343acc2e65e3f65926bcef1072
durga21131 <durgaprasad21131@gmail.com>
2024-12-30 22:36:50 +0530

    new

--- ArmStrongNumber.java
+++ ArmStrongNumber.java
@@ -3,6 +3,18 @@ package NumberProblems;
 import java.util.Scanner;
 
 public class ArmStrongNumber {
+    /* 
+        An Armstrong number (also known as a narcissistic number, pluperfect digital invariant, or PPDI) 
+        is a number that is equal to the sum of its digits each raised to the power of the number of digits.
+        Steps to Check an Armstrong Number:
+              1. Count the number of digits d.
+              2. Separate the digits of the number.
+              3. Raise each digit to the power d and compute the sum.
+              4. Check if the sum equals the original number.
+        Example:
+        153 = 1*1*1+5*5*5+3*3*3 = 1+125+27 =153
+        here number of digits is 3 (1,5,3) and individual digits are 1,5,3.
+    */
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
