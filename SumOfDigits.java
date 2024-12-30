316cd6c5a06b0127e52c1a4bd70bde83fe3982dd
durga21131 <durgaprasad21131@gmail.com>
2024-12-30 22:08:00 +0530

    new

---
+++ SumOfDigits.java
@@ -0,0 +1,22 @@
+package NumberProblems;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		long Number = sc.nextLong();
+		long Duplicate=Number;
+		int SOD = 0;
+		while (Number > 0) {
+			long remainder = Number % 10;
+			SOD+=remainder;
+			Number/=10;
+		}
+		System.out.println("The sum of individual digits of "+Duplicate+" is: "+SOD);
+
+	}
+
+}
