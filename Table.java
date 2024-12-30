098b9bc81706a1e9cfa1df43093fe9be6a775873
durga21131 <durgaprasad21131@gmail.com>
2024-12-30 22:09:31 +0530

    new

---
+++ Table.java
@@ -0,0 +1,19 @@
+package NumberProblems;
+
+import java.util.Scanner;
+
+public class Table {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number for a table");
+		int table=sc.nextInt();
+		System.out.println("please give start and stop values");
+		int start=sc.nextInt();
+		int stop=sc.nextInt();
+		for(int i=start;i<=stop;i++) {
+			System.out.println(table+" * "+i+" = "+ table*i);
+		}
+	}
+
+}
