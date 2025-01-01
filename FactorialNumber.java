41a0c90c23667cf6a525a2eb5b4c68027a4dc343
durga21131 <durgaprasad21131@gmail.com>
2025-01-01 20:53:47 +0530

    new

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -3,6 +3,11 @@ package NumberProblems;
 import java.util.Scanner;
 
 public class FactorialNumber {
+	/*
+	The factorial of a non-negative integer n, denoted as 𝑛!, is the product of all positive integers less than or equal to n.
+    It is mathematically defined as:
+	n!=n*(n-1)*(n-2)*....*2*1
+	*/
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
