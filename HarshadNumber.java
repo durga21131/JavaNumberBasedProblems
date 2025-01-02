50420ae9e465a4d289b83f1966803c843dcc036e
durga21131 <durgaprasad21131@gmail.com>
2025-01-02 21:15:26 +0530

    new

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -3,6 +3,13 @@ package NumberProblems;
 import java.util.Scanner;
 
 public class HarshadNumber {
+	/*
+	   A Harshad number (also known as a Niven number) is a positive integer that is divisible by the sum of its digits. 
+	   The term "Harshad" comes from the Sanskrit words "har" (meaning "joy") and "shad" (meaning "give"), collectively meaning "joy-giver." 
+	     Examples:
+            18: The sum of its digits is 1 + 8 = 9, and 18 is divisible by 9.
+			1729: The sum of its digits is 1 + 7 + 2 + 9 = 19, and 1729 is divisible by 19.
+	*/
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
