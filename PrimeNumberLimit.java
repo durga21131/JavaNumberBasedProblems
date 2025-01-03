b4448e030cdedfc3e8d4b5a5d76ed372396f85f5
durga21131 <durgaprasad21131@gmail.com>
2025-01-03 21:13:58 +0530

    new

---
+++ PrimeNumberLimit.java
@@ -0,0 +1,36 @@
+package NumberProblems;
+
+import java.util.Scanner;
+
+public class PrimeNumberLimit {
+
+    // Method to check if a number is prime
+    boolean isPrime(int number) {
+        if (number <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // Number is divisible by i, hence not prime
+            }
+        }
+        return true; // Number is prime
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter starting number:");
+        int start = sc.nextInt();
+        System.out.println("Enter ending number:");
+        int end = sc.nextInt();
+        sc.close();
+
+        PrimeNumberLimit p = new PrimeNumberLimit();
+        System.out.println("The prime numbers between " + start + " and " + end + " are:");
+        for (int i = start; i <= end; i++) { // Include 'end' in the range
+            if (p.isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
