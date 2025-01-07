9824fcc350485407ce95acc5fbe0d47977a49042
durga21131 <durgaprasad21131@gmail.com>
2025-01-07 21:33:54 +0530

    new

--- LeapYear.java
+++ LeapYear.java
@@ -10,12 +10,14 @@ public class LeapYear {
            2. Not divisible by 100: However, if the year is divisible by 100, it is not a leap year unless...
            3. Divisible by 400: If the year is divisible by 400, it is a leap year.
 		    a century year is also a leap if it is divisible by 400
+
 	*/
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number to check whether it is a leap year or not");
 		int year = sc.nextInt();
+
 		if (year % 4 == 0 && year % 100 != 0) {
 			System.out.println(year + " is a leap year");
 		} else if (year % 100 == 0 && year % 400 == 0) {
@@ -23,5 +25,6 @@ public class LeapYear {
 		} else {
 			System.out.println(year + " is not a leap year");
 		}
+		sc.close();
 	}
 }
