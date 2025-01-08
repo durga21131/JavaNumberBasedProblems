437d7911fb1c51230639ab03e4f463be00723b2f
durga21131 <durgaprasad21131@gmail.com>
2025-01-08 21:30:21 +0530

    new

--- LeapYear.java
+++ LeapYear.java
@@ -12,17 +12,18 @@ public class LeapYear {
 		    a century year is also a leap if it is divisible by 400
 
 	*/
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number to check whether it is a leap year or not");
 		int year = sc.nextInt();
-
+        
 		if (year % 4 == 0 && year % 100 != 0) {
 			System.out.println(year + " is a leap year");
-		} else if (year % 100 == 0 && year % 400 == 0) {
+		} 
+		else if (year % 100 == 0 && year % 400 == 0) {
 			System.out.println(year + " is a leap year");
-		} else {
+		} 
+		else {
 			System.out.println(year + " is not a leap year");
 		}
 		sc.close();
