a56e1da7b3cbcfb8ab1195ef7abeae16c3874bc7
durga21131 <durgaprasad21131@gmail.com>
2025-01-11 13:24:31 +0530

    new

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -23,24 +23,24 @@ public class PerfectSquare {
 		int OddSum=0;
 		int Square=0;
 		if(Number<0) {
-			System.out.println("Perfect square is not possible for negative numbers");
+			System.out.println( "Perfect square is not possible for negative numbers");
 		}
 		else {
-		for(int i=1;i<Number;i++) {
-			if (i%2==1){
-				OddSum+=i;
-				Square++;
-				if(OddSum==Number) {
-					break;
+		   for(int i=1;i<Number;i++) {
+			   if (i%2==1){
+				  OddSum+=i;
+				  Square++;
+				  if(OddSum==Number) {
+					 break;
 				}
 			}
-		}
+		  }
 		if(Number==OddSum) {
 			System.out.println(Number+" is a perfect square number of: "+Square);
 		}
 		else {
 			System.out.println(Number+" is not a perfect square number");
 		}
-		}
+	}
 	}
 }
