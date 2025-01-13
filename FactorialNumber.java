937e95e5f7257ea66fb4951b59bf99803030e09d
durga21131 <durgaprasad21131@gmail.com>
2025-01-13 20:27:38 +0530

    new

--- FactorialNumber.java
+++ FactorialNumber.java
@@ -12,15 +12,16 @@ public class FactorialNumber {
 	*/
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-        int Number=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+        int Number = sc.nextInt();
         int Factorial = 1;
         if(Number < 0) {
         	System.out.println("Factorial for negative numbers are undefined");
         }
         else if(Number == 0) {
         	System.out.println("Factorial for 0 is: 1");
-        }else {
+        }
+		else {
         	for(int i=1; i<=Number ;i++) {
         		Factorial *= i;
         	}
