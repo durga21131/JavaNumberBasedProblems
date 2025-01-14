6d2604552ef3b37bdf6380de6ede81987f062eb3
durga21131 <durgaprasad21131@gmail.com>
2025-01-14 20:11:15 +0530

    new

--- BinarytoDecimal.java
+++ BinarytoDecimal.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class BinarytoDecimal {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Binary Number");
 		// taking input as String
-		String StrBinary=sc.next();
-		int DecimalValue=0;
+		String StrBinary = sc.next();
+		int DecimalValue = 0;
 		// Convert StrBinary to Decimal value
 		for(int i=0;i<StrBinary.length();i++) {
-			char bit=StrBinary.charAt(StrBinary.length()-1-i); // accessing rightmost element 
+			char bit = StrBinary.charAt(StrBinary.length()-1-i); // accessing rightmost element 
 			if(bit=='1') {
 				DecimalValue+=Math.pow(2,i);
 			}
