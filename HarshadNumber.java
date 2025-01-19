588cca3fa9f06c8a4c322cb193c53603ca444ff1
durga21131 <durgaprasad21131@gmail.com>
2025-01-19 20:15:36 +0530

    new

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -14,19 +14,21 @@ public class HarshadNumber {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a number to check it is Harshad number or not");
-		int Number=sc.nextInt();
-		int Duplicate=Number;
-		int SOD=0;
+		int Number = sc.nextInt();
+		int Duplicate = Number;
+		int SOD = 0;
 		while(Number > 0) {
 			int remainder=Number%10;
 			SOD+=remainder;
 			Number/=10;
-		}if(Duplicate % SOD == 0) {
+		}
+		if(Duplicate % SOD == 0) {
 			System.out.println(Duplicate+" is a Harshad Number");
 		}
 		else {
 			System.out.println(Duplicate+" is not Harshad Number");
 		}
+		sc.close();
 	}
 
 }
