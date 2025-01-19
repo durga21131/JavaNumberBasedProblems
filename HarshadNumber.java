6da5494ed4455e7344eda8f69c5fddcf2caa9b8a
durga21131 <durgaprasad21131@gmail.com>
2025-01-19 20:16:16 +0530

    new

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -12,15 +12,15 @@ public class HarshadNumber {
 	*/
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number to check it is Harshad number or not");
 		int Number = sc.nextInt();
 		int Duplicate = Number;
 		int SOD = 0;
 		while(Number > 0) {
-			int remainder=Number%10;
-			SOD+=remainder;
-			Number/=10;
+			int remainder = Number%10;
+			SOD += remainder;
+			Number /= 10;
 		}
 		if(Duplicate % SOD == 0) {
 			System.out.println(Duplicate+" is a Harshad Number");
