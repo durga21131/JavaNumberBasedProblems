e9f9029bf1bf27ab1edc76e136b45c28f9df30e0
durga21131 <durgaprasad21131@gmail.com>
2025-01-21 20:24:49 +0530

    new

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -22,7 +22,12 @@ public class HarshadNumber {
 			SOD += remainder;
 			Number /= 10;
 		}
-		
+		if(Duplicate % SOD == 0) {
+			System.out.println(Duplicate+" is a Harshad Number");
+		}
+		else {
+			System.out.println(Duplicate+" is not Harshad Number");
+		}
 		sc.close();
 	}
 
