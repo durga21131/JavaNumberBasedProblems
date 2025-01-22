14028cc5fe5016b53dbdc36d5dfe42ba96f61f77
durga21131 <durgaprasad21131@gmail.com>
2025-01-22 20:15:27 +0530

    new

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -3,7 +3,19 @@ package NumberProblems;
 import java.util.Scanner;
 
 public class PerfectSquare {
-	
+	/* 
+	 * A perfect square number is a number that is the square of an integer. 
+	 * In other words, if a number n can be expressed as n=k*k(k square)
+	 * where k is an integer, then n is a perfect square.
+	 * Perfect square numbers are always non-negative because squaring any
+	 * integer (positive or negative) results in a non-negative value.
+	 * Sum of Consecutive Odd Numbers: Any perfect square is equal to the sum of 
+	 * the first n consecutive odd numbers. For example:
+	 * 1=1
+	 * 4=1+3
+	 * 9=1+3+5
+	 * 16=1+3+5+7
+	 */
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
