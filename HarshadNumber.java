eb5427107b6e424ad17e45ed4de57de009563022
durga21131 <durgaprasad21131@gmail.com>
2025-02-05 19:30:03 +0530

    new

--- HarshadNumber.java
+++ HarshadNumber.java
@@ -11,24 +11,5 @@ public class HarshadNumber {
 			1729: The sum of its digits is 1 + 7 + 2 + 9 = 19, and 1729 is divisible by 19.
 	*/
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a number to check it is Harshad number or not");
-		int Number = sc.nextInt();
-		int Duplicate = Number;
-		int SOD = 0;
-		while(Number > 0) {
-			int remainder = Number%10;
-			SOD += remainder;
-			Number /= 10;
-		}
-		if(Duplicate % SOD == 0) {
-			System.out.println(Duplicate+" is a Harshad Number");
-		}
-		else {
-			System.out.println(Duplicate+" is not Harshad Number");
-		}
-		sc.close();
-	}
-
+	
 }
