666906246a87376b350abc2fcab77fab6de4b099
durga21131 <durgaprasad21131@gmail.com>
2025-02-09 11:01:01 +0530

    new

--- Pallindrome.java
+++ Pallindrome.java
@@ -9,17 +9,7 @@ public class Pallindrome {
             Not Palindrome Numbers: 123, 10, 1234.
 	*/
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number to check whether it is a pallindrome or not");
-        int Number=sc.nextInt();
-        int Duplicate=Number;
-        int ReverseNumber=0;
-        while(Number>0) {
-        	int remainder=Number%10;
-        	ReverseNumber=ReverseNumber*10+remainder;
-        	Number/=10;
-		// checking pallindrome or not
+	
         }if(Duplicate==ReverseNumber) {
         	System.out.println(Duplicate+" is a pallindrome number");
         }else {
