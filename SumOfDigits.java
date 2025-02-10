99014ba3227277ffadc2246d88757cce1da836a6
durga21131 <durgaprasad21131@gmail.com>
2025-02-10 20:44:11 +0530

    push

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -2,7 +2,23 @@ package NumberProblems;
 
 import java.util.Scanner;
 
-public 
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		long Number = sc.nextLong();
+		sc.close();
+		long Duplicate = Number;
+		int SOD = 0;
+		//checking number of digits
+		while (Number > 0) {
+			long remainder = Number % 10;
+			SOD += remainder;
+			Number /= 10;
+		}
+		System.out.println("The sum of individual digits of "+Duplicate+" is: "+SOD);
+
 	}
 
 }
