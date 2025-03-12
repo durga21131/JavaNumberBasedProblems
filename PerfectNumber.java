e626f65e1f39e6aba08c43790f90941716b45cce
durga21131 <durgaprasad21131@gmail.com>
2025-03-12 21:00:26 +0530

    new

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -11,18 +11,6 @@ public class PerfectNumber {
 	The first four perfect numbers are 6, 28, 496, and 8128.
 	*/
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number to check it is Perfect number or not");
-		int Number=sc.nextInt();
-		int Sum=0;
-		for(int i=1;i<Number;i++) {
-			if(Number%i==0) {
-				Sum+=i;
-			}
-		}if(Sum==Number) {
-			System.out.println(Number+" is a perfect Number");
-		}else {
 			System.out.println(Number+ " is not a perfect Number");
 		}
 
