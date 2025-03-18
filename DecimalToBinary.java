239d78404b1c783712b14e48c75f65f3bca02522
durga21131 <durgaprasad21131@gmail.com>
2025-03-18 22:06:55 +0530

    new

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -7,7 +7,15 @@ public class DecimalToBinary {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a decimal number");
-        
+        int Number = sc.nextInt();
+        int Duplicate = Number;
+        int arr[] = new int[32];
+        int i=0;
+        while(Number > 0) {
+        	arr[i] = Number%2;
+        	i++;
+        	Number /= 2;
+        }
         System.out.println("The binary form of "+ Duplicate +" is:");
         for(int j = i-1 ;j >= 0; j--) {
         	System.out.print(arr[j]+" ");
